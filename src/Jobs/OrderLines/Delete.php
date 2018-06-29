50c9a819755e4a7cf91715153b9766f4076b1fcc
Alec Ritson <alec@neondigital.co.uk>
2018-06-29 15:59:38 +0100

    Added jobs

---
+++ src/Jobs/OrderLines/Delete.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace GetCandy\Client\Jobs\OrderLines;
+
+use GetCandy\Client\AbstractJob;
+
+class Delete extends AbstractJob
+{
+    protected $method = "DELETE";
+    protected $endpoint = 'orders/lines/{id}';
+    protected $handle = 'orders-add-line';
+    protected $idField = 'order_id';
+}
