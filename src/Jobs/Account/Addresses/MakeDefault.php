eb636b0b4931e3bd5bfbe2ad4d17266b0faae69f
Jamie Warburton <jamie@hexdigital.com>
2019-06-03 15:23:39 +0100

    Add make + remove default address jobs

---
+++ src/Jobs/Account/Addresses/MakeDefault.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace GetCandy\Client\Jobs\Account\Addresses;
+
+use GetCandy\Client\AbstractJob;
+
+class MakeDefault extends AbstractJob
+{
+    protected $endpoint = 'addresses/{id}/makeDefault';
+    protected $handle = 'make-default-address';
+    protected $method = 'POST';
+}
